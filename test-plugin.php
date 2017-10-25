2bc0003bd40262dd46325647825110ee3e391b71
cuongdt90 <cuongdt90@gmail.com>
2017-10-25 11:14:22 +0700

    update comment
    update description

---
+++ test-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Test plugin
+Description: This is a test plugin
+Author: Ahihi
+Version: 0.1
+Text Domain:
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
